cf15b4be1346c62dc787a0e733673bc00333a80c
Alexandra Bubnova <alexandra.slob@gmail.com>
2023-10-01 20:58:05 +0200

    feat: add header-scroll shadow

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,13 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== SHADOW HEADER ===============*/
 
+const shadowHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+        : header.classList.remove('shadow-header')
+}
+
+window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
 
